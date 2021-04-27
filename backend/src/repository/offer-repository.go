fe2d4e2c727ac42e24d58a41aa63f104abcb7294
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-28 01:12:36 +0200

    method to get manually created offer deleted

--- backend/src/repository/offer-repository.go
+++ backend/src/repository/offer-repository.go
@@ -6,7 +6,6 @@ import (
 	"model"
 	"time"
 
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -254,49 +253,4 @@ func CheckErrorOffer(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-/*
-	manually create and return offer
-*/
-func getOffer() model.Offer {
-	const layout = "2006-01-02"
-	d, _ := time.Parse(layout, "2020-05-05")
-
-	var offer model.Offer
-	offer.Id = uuid.New().String()
-	offer.Location = "Novi Sad"
-	offer.Date = d
-	offer.Price = 20000
-
-	var vehicle model.Vehicle
-
-	vehicle.Id = "1"
-	vehicle.Make = "BMW"
-	vehicle.ModelCar = "320"
-	vehicle.HP = 150
-	vehicle.Cubic = 2000
-
-	vehicle.Date = d
-
-	// add vehicle to offer
-	offer.Vehicle = vehicle
-
-	var comment1 model.Comment
-	comment1.Id = uuid.New().String()
-	comment1.OfferId = offer.Id
-	comment1.Content = "com 1"
-
-	// add comment to offer
-	offer.Comments = append(offer.Comments, comment1)
-
-	var rate1 model.Rate
-	rate1.Id = uuid.New().String()
-	rate1.OfferId = offer.Id
-	rate1.Mark = 3
-
-	// add rate to offer
-	offer.Rates = append(offer.Rates, rate1)
-
-	return offer
 }
