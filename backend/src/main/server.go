03174025574ce98c3b38d341f3e4d99ad10b0133
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-27 15:36:53 +0200

    Create server.go

---
+++ backend/src/main/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"controller"
+	"fmt"
+	"net/http"
+	"repository"
+	"service"
+
+	"router"
+)
+
+var (
+	vehicleRepository repository.VehicleRepository = repository.NewVehicleRepository()
+	vehicleService    service.VehicleService       = service.NewVehicleService(vehicleRepository)
+	vehicleController controller.VehicleController = controller.NewVehicleController(vehicleService)
+	httpRouter        router.Router                = router.NewMuxRouter()
+)
+
+func runServer() {
+	const port string = ":8000"
+
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Up and runing")
+	})
+
+	httpRouter.GET("/vehicles", vehicleController.GetAll)
+	httpRouter.POST("/vehicles", vehicleController.Save)
+	httpRouter.DELETE("/vehicles/delete-all", vehicleController.DeleteAll)
+
+	httpRouter.SERVE(port)
+}
